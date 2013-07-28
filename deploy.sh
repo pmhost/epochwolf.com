ddbe7fcbd2399d87a1dd01abb34f7a176dfad658
epochwolf <epochwolf@gmail.com>
2013-07-28 13:54:21 -0700

    Removing shell scripts.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BuildScript="build.sh"
-BuildFolder="build/"
-RemoteFolder="kazan:/srv/blog/"
-LogFile=deploy.log
-
-rsync --recursive --links --verbose --rsh=ssh --delete $BuildFolder $RemoteFolder
